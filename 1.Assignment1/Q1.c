7822fb86e9a62d430bb6d48677f164b285c39fb4
prabhas2127 <prafulbr20@gmail.com>
2025-02-09 18:55:41 +0530

    .

--- 1.Assignment1/Q1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("Name is:Priyanka Satdive\n");
-    printf("Address:Aurngabad,maharastra\n");
-    printf("City:Aurangabad\n");
-    return 0;
-}
--- 1.Assignment1/Q2.c
+++
@@ -1,6 +0,0 @@
-# include <stdio.h>
-int main(){
-    printf("Name:Priyanka\n\n");
-    printf("Address:Aurangabad\n\n");
-
-}

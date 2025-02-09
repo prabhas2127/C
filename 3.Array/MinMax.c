e44323cf9fda2d718222992a59f8c7084da80c4e
prabhas2127 <prafulbr20@gmail.com>
2025-02-09 19:43:28 +0530

    Update MinMax.c

--- 3.Array/MinMax.c
+++ 3.Array/MinMax.c
@@ -1,35 +1,40 @@
 #include<stdio.h>
+
 int main(){
-    int a[5],i,max;
-    printf("enter values:");
-    //accept 5 elements
-    for(i=0;i<5;i++){
+
+    int a[6], val, min_val, max_val;
+
+    printf("enter array elements : \n");
+
+    for(int i=0;i<6;i++){
         scanf("%d",&a[i]);
     }
-    //print array
-    printf("array is:");
-    for (i = 0; i < 5; i++)
-    {
-        
-        printf("%d  ",a[i]);
-    }
-    printf("enter max value:\n");
-    scanf("%d",&max);
 
-    for (i = 0; i < 5; i++)
-    {
-        if(a[i] <= max){
-            printf("min no :%d \n",a[i]);
-        }
-        else{
-            printf("max no: %d\n",a[i]);
-        }
-
-        
+    printf("input array : \n");
+    for(int i=0;i<6;i++){
+        printf("%d\t",a[i]);
     }
-    
+    printf("\n");
+
+    printf("enter a ref value : ");
+    scanf("%d",&val);
+
+    min_val = a[0];
+    max_val = a[0];
+
+    for(int i=0;i<6;i++){
+        if(a[i] < min_val){
+            min_val = a[i];
+        }
+        if(a[i] > max_val){
+            max_val = a[i];
+        }
+    }
+
+    printf("minimum value among the array : %d\n", min_val);
+    printf("maximum value among the array : %d\n", max_val);
 
     return 0;
-    
 
 }
+
